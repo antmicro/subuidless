e0e7069ed85d5502c79dde1ad3935f2ba00ab093
Peter Gielda <pgielda@antmicro.com>
2021-07-21 16:13:22 +0200

    static compilation
    compile statically
    also use systems libseccomp if the custom one is not found

--- make.sh
+++ make.sh
@@ -2,7 +2,15 @@
 # FIXME: use autoconf/automake
 
 # requires libseccomp >= v2.5.0
-: ${LIBSECCOMP_PREFIX:=/opt/libseccomp}
+: ${LIBSECCOMP_PREFIX:=/opt/libseccomp/lib}
+LIBSECCOMP="${LIBSECCOMP_PREFIX}/libseccomp.a"
+
+if [ ! -e ${LIBSECCOMP} ] ; then
+	LIBSECCOMP=`whereis libseccomp | tr ' ' '\n' | grep \.a$`
+fi
+echo "Using ${LIBSECCOMP}..."
 
 set -eux -o pipefail
-gcc -o subuidless -I${LIBSECCOMP_PREFIX}/include *.c pb/*.c ${LIBSECCOMP_PREFIX}/lib/libseccomp.a -lprotobuf-c
+gcc -static -o subuidless -I${LIBSECCOMP_PREFIX}/include *.c pb/*.c ${LIBSECCOMP} -lprotobuf-c
+
+strip subuidless
